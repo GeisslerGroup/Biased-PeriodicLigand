58950d789043a3b254c53a1ee1f588f3e96638d6
Pratima Satish <pratimasatish@berkeley.edu>
2016-08-17 10:39:30 -0700

    added option to python script of specifying window centre instead of hardcoding

--- printcosine.py
+++ printcosine.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("-bias", type=float, help="bias value for spring constant")
+parser.add_argument("-bias", type=float, default=30000.0, help="bias value for spring constant")
+parser.add_argument("-at", type=float, default=-0.262, help="average value of order parameter in biased simulation")
 args   = parser.parse_args()
 
 str_out = ""
@@ -37,9 +38,9 @@ for i in range(1, count):
     str_out = str_out + "0.00416667,"
 str_out = str_out.rstrip(",")
 
-str_out = str_out + " PERIODIC=NO"
+str_out = str_out + " PERIODIC={-pi,pi}"
 
-str_out = str_out + "\n\nRESTRAINT ARG=avgtheta AT=-0.262 KAPPA={} LABEL=restraint\n\nPRINT ARG=avgtheta STRIDE=10\n".format(args.bias)
+str_out = str_out + "\n\nRESTRAINT ARG=avgtheta AT={} KAPPA={} LABEL=restraint\n\nPRINT ARG=avgtheta STRIDE=10\n".format(args.at, args.bias)
 
 str_out = str_out.rstrip(",")
 print str_out
--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@
 # mpiexec -np 6 ~/local/lammps-plumed-2Apr13/src/lmp_openmpi -in in_new.340
 
 biaslist=$(seq 25000 5000 75000)
+at=-0.262
 
 for bias in ${biaslist}
 do
-	python printcosine.py -bias ${bias} > cosine.plu
+	python printcosine.py -bias ${bias} -at ${at }> cosine.plu
 	lmp_plumed -in in.long -log log${bias}.lammps
+	cp dump.340 dump.340.${bias}
 done
