eab16e0cdd11a4559170ca9aaa156bf5c9deab43
Pratima Satish <pratimasatish@berkeley.edu>
2016-08-17 18:50:24 -0700

    adding some untracked changes

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ at=-0.262
 
 for bias in ${biaslist}
 do
-	python printcosine.py -bias ${bias} -at ${at }> cosine.plu
+	python printcosine.py -bias ${bias} -at ${at} > cosine.plu
 	lmp_plumed -in in.long -log log${bias}.lammps
 	cp dump.340 dump.340.${bias}
 done
