5e625e0dd7703005c8606dd2d302fb769f204a4d
Pratima Satish <pratimasatish@berkeley.edu>
2016-09-13 15:44:30 -0700

    added a gradual moving restraint in PLUMED

--- printcosine.py
+++ printcosine.py
@@ -4,18 +4,22 @@ import argparse
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("-bias", type=float, default=2500.0, help="bias value for spring constant")
-parser.add_argument("-at", type=float, default=-0.262, help="average value of order parameter in biased simulation")
+parser.add_argument("-at_lower", type=float, default=-0.75, help="lower limit of order parameter in biased simulation")
+parser.add_argument("-at_upper", type=float, default=-0.25, help="upper limit of order parameter in biased simulation")
+parser.add_argument("-at_step", type=float, default=0.05, help="step between each consecutive umbrella")
+parser.add_argument("-timestep", type=int, default=5000, help="timesteps after which umbrella is moved")
 args   = parser.parse_args()
 
+at_range = np.arange(args.at_lower, args.at_upper+args.at_step, args.at_step)
 str_out = ""
 count = 1
 
 for i in range(3840, 6000, 18):
-    str_out = str_out + "d{}: DISTANCE ATOMS={},{} COMPONENTS\n".format(count, i, i+12)
+    str_out = str_out + "d{}: DISTANCE ATOMS={},{} COMPONENTS\n".format(count, i, i+9)
     count = count + 1
 
 for i in range(8160, 10320, 18):
-    str_out = str_out + "d{}: DISTANCE ATOMS={},{} COMPONENTS\n".format(count, i, i+12)
+    str_out = str_out + "d{}: DISTANCE ATOMS={},{} COMPONENTS\n".format(count, i, i+9)
     count = count + 1
 
 for i in range(1, count):
@@ -40,7 +44,12 @@ str_out = str_out.rstrip(",")
 
 str_out = str_out + " PERIODIC={-pi,pi}"
 
-str_out = str_out + "\n\nRESTRAINT ARG=avgtheta AT={} KAPPA={} LABEL=restraint\n\nPRINT ARG=avgtheta STRIDE=10\n".format(args.at, args.bias)
+str_out = str_out + "\n\nMOVINGRESTRAINT ...\n"
+str_out = str_out + "  ARG=avgtheta\n"
+for i in range(len(at_range)):
+    str_out = str_out + "  STEP{}={} AT{}={} KAPPA{}={}\n".format(i, i*args.timestep, i, at_range[i], i, args.bias)
+str_out = str_out + "... MOVINGRESTRAINT\n"
 
+str_out = str_out + "\nPRINT ARG=avgtheta STRIDE=10\n"
 str_out = str_out.rstrip(",")
 print str_out
