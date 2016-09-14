22b03caa7daa71c46d59b4c9e1773e75c8888d64
Pratima Satish <pratimasatish@berkeley.edu>
2016-09-14 14:51:29 -0700

    writing PLUMED file for using moving restraint on complicated theta_z

--- printtheta.py
+++ printtheta.py
@@ -4,8 +4,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("-bias", type=float, default=2500.0, help="bias value for spring constant")
-parser.add_argument("-at", type=float, default=-0.262, help="average value of order parameter in biased simulation")
+parser.add_argument("-at_lower", type=float, default=-0.75, help="lower limit of order parameter in biased simulation")
+parser.add_argument("-at_upper", type=float, default=-0.25, help="upper limit of order parameter in biased simulation")
+parser.add_argument("-at_step", type=float, default=0.05, help="step between each consecutive umbrella")
+parser.add_argument("-timestep", type=int, default=5000, help="timesteps after which umbrella is moved")
 args   = parser.parse_args()
+
+at_range = np.arange(args.at_lower, args.at_upper+args.at_step, args.at_step)
 
 str_out = ""
 count = 1
@@ -56,7 +61,16 @@ str_out = str_out.rstrip(",")
 
 str_out = str_out + " PERIODIC={-pi,pi}"
 
-str_out = str_out + "\n\nRESTRAINT ARG=avgtheta AT={} KAPPA={} LABEL=restraint\n\nPRINT ARG=avgtheta STRIDE=10\n".format(args.at, args.bias)
+# str_out = str_out + "\n\nRESTRAINT ARG=avgtheta AT={} KAPPA={} LABEL=restraint\n\nPRINT ARG=avgtheta STRIDE=10\n".format(args.at, args.bias)
+
+str_out = str_out + "\n\nMOVINGRESTRAINT ...\n"
+str_out = str_out + "  ARG=avgtheta\n"
+for i in range(len(at_range)):
+    str_out = str_out + "  STEP{}={} AT{}={} KAPPA{}={}\n".format(i, i*args.timestep, i, at_range[i], i, args.bias)
+str_out = str_out + "... MOVINGRESTRAINT\n"
+
+str_out = str_out + "\nPRINT ARG=avgtheta STRIDE=10\n"
+
 
 str_out = str_out.rstrip(",")
 print str_out
