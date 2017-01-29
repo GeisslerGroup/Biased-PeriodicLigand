a66df698f89c534b066151f0ad41db4318d7c0d6
Pratima Satish <pratimasatish@berkeley.edu>
2017-01-28 17:47:39 -0800

    adding functionality to calculate theta_x in addition to theta_z

---
+++ dump_files/xcos_th.py
@@ -0,0 +1,77 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+temp = 340.0
+
+parser = argparse.ArgumentParser(description="")
+# parser.add_argument("-bias", type=str, help="bias value to analyse")
+parser.add_argument("-log", action='store_true', help="plot log of probability")
+args = parser.parse_args()
+
+save = "hist.png"
+
+data = np.genfromtxt('/home/pratima/Biased-PeriodicLigand/dump_files/lig.-0.50', delimiter=' ')
+
+size = len(data)
+x0 = 0.0
+y0 = 0.0
+z0 = 0.0
+x1 = 0.0
+y1 = 0.0
+z1 = 0.0
+f = open("theta_x-0.50.txt","w")
+xb0 = 0.0
+yb0 = -54.7217
+zb0 = 0.0
+xb1 = 82.4293
+yb1 = 54.7217
+zb1 = 81.0040
+
+start = 500 * 18 * 240
+start = 0
+end = size
+# end = 100 * 18 * 240
+
+for i in range (start,end,18):
+  if (i + 17 < size):
+    x0 = data[i,1]
+    y0 = data[i,2]
+    z0 = data[i,3]
+    x1 = data[i+9,1]
+    y1 = data[i+9,2]
+    z1 = data[i+9,3]
+    x_vec = (x1 - x0) / (xb1 - xb0)
+    y_vec = y1 - y0
+    z_vec = (z1 - z0) / (zb1 - zb0)
+    z_vec = (z_vec - round(z_vec)) * (zb1 - zb0)
+    x_vec = (x_vec - round(x_vec)) * (xb1 - xb0)
+    thx = np.arctan(x_vec / y_vec)
+    f.write("%4.5f\n" %(thx))
+
+f.close()
+
+hist_data = np.genfromtxt('/home/pratima/Biased-PeriodicLigand/dump_files/theta_x-0.50.txt', delimiter=' ')
+# unbiased_data = np.genfromtxt('/home/pratima/Biased-PeriodicLigand/dump_files/zangle_distr_top.340', delimiter=' ')
+# unbiased_data = -unbiased_data * np.pi / 180.0
+
+# print np.mean(unbiased_data)
+# print np.std(unbiased_data)
+print np.mean(hist_data)
+print np.std(hist_data)
+bins = np.linspace(-1.70, 1.70, 200)
+hist, bins = np.histogram(hist_data, bins = bins, density = True)
+# unbiased_hist, bins = np.histogram(unbiased_data, bins = bins, density = True)
+bin_centres = bins[1:] * 0.5 + bins[:-1] * 0.5
+plt.figure()
+if args.log:
+    bin_centres = bin_centres[hist != 0]
+    hist = hist[hist != 0]
+    plt.plot(bin_centres, -np.log(hist))
+else:
+    plt.plot(bin_centres, hist, color='blue')
+#     plt.plot(180.0*bin_centres/np.pi, unbiased_hist, color='red')
+plt.show()
+# plt.savefig(save)
+
